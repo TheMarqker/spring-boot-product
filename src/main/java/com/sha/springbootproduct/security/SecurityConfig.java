e739d5d56cf006f19f55ee4884cd0d07c733626b
Adam Marquebreuck <61724267+TheMarqker@users.noreply.github.com>
2023-08-16 06:03:43 -0400

    Update SecurityConfig.java

--- src/main/java/com/sha/springbootproduct/security/SecurityConfig.java
+++ src/main/java/com/sha/springbootproduct/security/SecurityConfig.java
@@ -1,67 +1,86 @@
 package com.sha.springbootproduct.security;
 
-import com.sha.springbootproduct.model.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import com.sha.springbootproduct.model.Role;
+import com.sha.springbootproduct.security.jwt.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Collection;
-import java.util.Set;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class UserPrinciple implements UserDetails
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
-    private Long id;
-    private String username;
-    transient private String password; // don't show up on serialized places.
-    transient private User user; // user for only login operation, don't use in JWT.
-    private Set<GrantedAuthority> authorities;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities()
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-        return authorities;
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
-    public String getPassword()
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception
     {
-        return password;
+        return super.authenticationManagerBean();
     }
 
     @Override
-    public String getUsername()
+    protected void configure(HttpSecurity http) throws Exception
     {
-        return username;
+        http.cors();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                .antMatchers("/api/authentication/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/product").permitAll()
+                .antMatchers("/api/product/**").hasRole(Role.ADMIN.name())
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
-    public boolean isAccountNonExpired()
+    //Why don't we describe it as a component, because of scope.
+    @Bean
+    public JWTAuthorizationFilter jwtAuthorizationFilter()
     {
-        return true;
+        return new JWTAuthorizationFilter();
     }
 
-    @Override
-    public boolean isAccountNonLocked()
+    @Bean
+    public PasswordEncoder passwordEncoder()
     {
-        return true;
+        return new BCryptPasswordEncoder();
     }
 
-    @Override
-    public boolean isCredentialsNonExpired()
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
     {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled()
-    {
-        return true;
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addCorsMappings(CorsRegistry registry)
+            {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
